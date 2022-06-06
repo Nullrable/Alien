8086e01472d87617fa6e6c3c7c7b3375f9e54dad
lusudong <nhsoft.lsd@hotmail.com>
2022-06-06 11:40:09 +0800

    first commit

---
+++ src/test/java/org/lsd/alien/AlienApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lsd.alien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlienApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
